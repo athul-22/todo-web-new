c32bd7f74787f76c61f57e4fab939b9702fa3c52
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-02-24 17:42:26 +0530

    V5

--- src/Profile.js
+++ src/Profile.js
@@ -3,12 +3,27 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { useGoogleOneTapLogin } from 'react-google-one-tap-login';
 import './App.css';
-
+import GoogleIcon from '../src/images/GOOGLE.webp'
 
 function Profile() {
 
-    // GOOGLE SIGNIN 
+    // IF ELSE LOGIN CHECKING FUNCTION
     const [name, setName] = useState("");
+   var nameDB = localStorage.getItem("nameDB");
+   var profileDB = localStorage.getItem("pictureDB");
+   var emailDB = localStorage.getItem("emailDB");
+
+    if (name === "") {
+        document.getElementById("profile").src={GoogleIcon};
+    }
+    if(nameDB === ""){
+        document.getElementById("profile").src={GoogleIcon};
+    }else{
+        
+        document.getElementById("profile").src={profileDB};
+    }
+    // GOOGLE SIGNIN 
+  
     const [email, setEmail] = useState("");
     const [profile, setProfile] = useState("");
 
@@ -18,13 +33,14 @@ function Profile() {
     useGoogleOneTapLogin({
         onError: error => {
             console.log(error);
-
         },
         onSuccess: response => {
             setName(response.name);
             setEmail(response.email);
             setProfile(response.picture);
-            console.log(response);
+            localStorage.setItem("nameDB", JSON.stringify(response.name));
+            localStorage.setItem("emailDB", JSON.stringify(response.email));
+            localStorage.setItem("pictureDB", JSON.stringify(response.picture));
         },
         googleAccountConfigs: {
             client_id: "938054737950-90jhm2ntnupbngaf66rsg0k0b4qi6mkr.apps.googleusercontent.com"
@@ -34,13 +50,14 @@ function Profile() {
 
     const [show, setShow] = useState(false);
 
+
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
     return (
         <>
             <div onClick={handleShow} className='account'>
-                <img onClick={handleShow} id='profile' src={profile} alt="" height="50px" width="50px" />
+                <img onClick={handleShow} id='profile' src={GoogleIcon} alt="" height="50px" width="50px" />
             </div>
 
             <Modal show={show} onHide={handleClose}>
@@ -48,17 +65,14 @@ function Profile() {
                     <Modal.Title>My Account</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                   <center>
-                    <img onClick={handleShow} id='profile' src={profile} alt="" height="50px" width="50px" />
-                    <p id='name'>{name}</p>
-                    <p id='email'>{email}</p>
-                    </center> 
+                    <center>
+                        <img onClick={handleShow} id='profile' src={GoogleIcon} alt="" height="50px" width="50px" />
+                        <p id='name'>{name}</p>
+                        <p id='email'>{email}</p>
+                    </center>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>Close</Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
                 </Modal.Footer>
             </Modal>
         </>
