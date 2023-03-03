99c02acdc8977180575a24280ee21f0e89afaa7c
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-03 12:17:31 +0530

    V10

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apikey: "",
+    apikey: "AIzaSyAl9sbbgwxkyeh1rfQMVxbPnvNvX6SlS4s",
     authDomain:"",
     projectId: "",
     storageBucket:"",
-    appId:""
+    appId:"todoapp-fb470"
 });
 
 const db = firebaseApp.firestore();
