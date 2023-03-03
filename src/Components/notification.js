c1f2bbb7ee58e16d4d902128d0128864a2bd6eff
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-03-03 12:55:58 +0530

    v111

--- src/Components/notification.js
+++ src/Components/notification.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import db from './Firebase';
 import '../App.css';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import db from './Firebase';
 import notificationIcon from '../images/notifcationicon.png';
 import notifMain from '../images/notifmain.png';
 import notifinsideImg from '../images/EmptyInbox.png';
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
+    
     apiKey: "AIzaSyAl9sbbgwxkyeh1rfQMVxbPnvNvX6SlS4s",
     authDomain: "todoapp-fb470.firebaseapp.com",
     projectId: "todoapp-fb470",
     storageBucket: "todoapp-fb470.appspot.com",
     messagingSenderId: "453121987629",
     appId: "1:453121987629:web:923cc5722fa474bef314a4"
+
 });
-
 const db = firebaseApp.firestore();
-
 export default db;
